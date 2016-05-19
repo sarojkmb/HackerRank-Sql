a9207e292ded85d6265d086bd410efb11e824ced
Saroj Kumar Behera <sarojkmb@gmail.com>
2016-05-19 17:25:36 -0400

    Update Aggregation.sql

--- Aggregation.sql
+++ Aggregation.sql
@@ -11,6 +11,17 @@ select avg(population) from city where district='California';
 
 
 
+# Weather Observation Station 14
+select ROUND(MAX(LAT_N),4) from station where LAT_N<137.2345;
+
+# Weather Observation Station 15
+select ROUND(LONG_W, 4) from station where LAT_N= (select MAX(LAT_N) from station where LAT_N<137.2345);
+
+# Weather Observation Station 16
+select ROUND(MIN(LAT_N),4) from station where LAT_N>38.7780;
+
+
+
 
 
 
