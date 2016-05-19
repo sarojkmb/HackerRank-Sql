6f1be2024469a9b2720a8c64e702fcc2d942f856
Saroj Kumar Behera <sarojkmb@gmail.com>
2016-05-19 17:57:37 -0400

    Update Aggregation.sql

--- Aggregation.sql
+++ Aggregation.sql
@@ -20,6 +20,16 @@ select ROUND(LONG_W, 4) from station where LAT_N= (select MAX(LAT_N) from statio
 # Weather Observation Station 16
 select ROUND(MIN(LAT_N),4) from station where LAT_N>38.7780;
 
+# Weather Observation Station 17
+select ROUND(LONG_W,4) from station where LAT_N = (select MIN(LAT_N) from station where LAT_N>38.7780);
+
+# Weather Observation Station 18
+SELECT ROUND(ABS(A - C) + ABS(B - D), 4)
+FROM (
+		SELECT MAX(LAT_N) AS A, MIN(LAT_N) AS B , MAX(LONG_W) AS C , MIN(LONG_W) AS D
+		FROM STATION
+	) as st ;
+
 
 
 
